5e52762dce81f69acddc5760d65eb79c37aa8ab4
Niccolo <nikodavi55@gmail.com>
2026-01-29 21:09:21 +0100

    added some visualization tools in MacrospinSystem.dynamics()

--- LS_macrospinLib.py
+++ LS_macrospinLib.py
@@ -8,6 +8,7 @@ gamma0= 1.76e11   #[s T]^-1
 hbar = 	1.054571817e-34 #[J s]
 muB = 9.2740100657e-24 #[J T-1]
 q_e = 1.60217663e-19 #Coulomb
+mu0=4*np.pi*1e-7
 
 
 def get_vector(theta,phi):
@@ -28,23 +29,41 @@ def normConstraint(x,norm):
     return x[0]**2+x[1]**2+x[2]**2-norm
 
 class macrospinSystem():
-    def __init__(self, gl=1, gs=2, muB_S=1, muB_L=0, gammaL=0.5*gamma0, gammaS=gamma0, alphaS=0, alphaL=0):
+    def __init__(self, gl=1, gs=2, muB_S=1, muB_L=0, gammaL=0.5*gamma0, gammaS=gamma0, alphaS=0, alphaL=0, Ms=1e6, SOC_sign=1):
         Sx, Sy, Sz = syp.symbols(r"S_x,S_y,S_z")
         Lx, Ly, Lz = syp.symbols(r"L_x,L_y,L_z")
-        g_l,g_s,gamma_L, gamma_S, alpha_L, alpha_S = syp.symbols(r"g_l,g_s,gamma_L, gamma_S, alpha_S, alpha_L")
+        M_s, fL, fS        = syp.symbols(r"M_s, f_L, f_S")
+        #g_l,g_s,gamma_L, gamma_S, alpha_L, alpha_S = syp.symbols(r"g_l,g_s,gamma_L, gamma_S, alpha_S, alpha_L")
         
+        
+        #define spin and orbital moments
         self.S = syp.Matrix([Sx,Sy,Sz])
         self.L = syp.Matrix([Lx,Ly,Lz])
         
-
+        #define fractions of magnetization of L and S 
+        mu_total=np.abs(muB_S+SOC_sign*muB_L)
+        f_S=muB_S/mu_total
+        f_L=muB_L/mu_total
+        self.ML=fL*self.L*M_s
+        self.MS=fS*self.S*M_s
+        #define magnetization
+        self.M=self.ML+self.MS
         
-        self.M=g_l*self.L+g_s*self.S
+        self.niceML=syp.MatMul(fL,M_s,self.L, evaluate=False)
+        self.niceMS=syp.MatMul(fS,M_s,self.S, evaluate=False)
+        self.niceM=syp.MatAdd(self.niceML,self.niceMS,evaluate=False)
+        self.nicefield=[]
+        self.nicefieldL=[]
+        self.nicefieldS=[]
         
+        #initialize energy and field(s) 
         self.field=syp.Matrix([0,0,0])
         self.energy=0
         self.field_L=syp.Matrix([0,0,0])
         self.field_S=syp.Matrix([0,0,0])
+
         
+        #inizialize parameters (constants throughout), variables (experimentally controllable elements), and unknowns (L and S directions, but not magnitude)
         self.pars={}
         self.variables={}
         if muB_L==0:
@@ -63,34 +82,45 @@ class macrospinSystem():
         self.pars["alpha_S"]=alphaS
         self.pars["muB_L"]=muB_L
         self.pars["muB_S"]=muB_S
+        self.pars["M_s"]=Ms
+        self.pars["f_L"]=f_L
+        self.pars["f_S"]=f_S
         
     def externalField(self, B0=0, uB=[1,0,0]):
         
         Bext,ubx,uby,ubz = syp.symbols(r"B_ext, u_bx, u_by, u_bz")
         
         B_ext=Bext*syp.Matrix([ubx,uby,ubz])
+        nicefield=syp.MatMul(Bext,syp.Matrix([ubx,uby,ubz]), evaluate=False)
+        
+
         
         if "B_ext" not in self.variables:
             self.field+=B_ext
             self.energy+=-B_ext.dot(self.M)
-            
-        
+            self.nicefield.append(nicefield)
+       
         self.variables[r"B_ext"]=B0
         self.variables['u_bx']=uB[0]
         self.variables['u_by']=uB[1]
         self.variables['u_bz']=uB[2]
     
-    def demagField(self,mu0_Ms,N_diag):
-        Bdem, Nxx, Nyy, Nzz = syp.symbols(r'B_dem,N_xx,N_yy,N_zz')
+    def demagField(self,N_diag):
+        mu0, Ms, Nxx, Nyy, Nzz = syp.symbols(r'mu_0, M_s,N_xx,N_yy,N_zz')
         N=syp.diag(Nxx,Nyy,Nzz)
         
-        B_dem=-Bdem*N@self.M
+        B_dem=-N@self.M
         
-        if "B_dem" not in self.pars:
+        #niceN=syp.Matrix([[Nxx,0,0],[0,Nyy,0],[0,0,Nzz]])
+
+        nicefield=-syp.MatMul(N,self.niceM, evaluate=False)
+
+        
+        if "N_xx" not in self.pars:
             self.field+=B_dem
             self.energy+=-0.5*B_dem.dot(self.M)
-
-        self.pars["B_dem"]=mu0_Ms
+            self.nicefield.append(nicefield)
+        
         self.pars["N_xx"]=N_diag[0]
         self.pars["N_yy"]=N_diag[1]
         self.pars["N_zz"]=N_diag[2]
@@ -99,14 +129,18 @@ class macrospinSystem():
         Bani,ukx,uky,ukz = syp.symbols(r"B_k, u_kx, u_ky, u_kz")
         
         B_ani=Bani*syp.Matrix([ukx,uky,ukz])
+        nicefield=syp.MatMul(Bani,syp.Matrix([ukx,uky,ukz]),evaluate=False)
         
         if "B_k" not in self.pars:
             if "L_x" not in self.unknowns:
                 self.field+=B_ani
+                self.nicefield.append(nicefield)
             else:
                 self.field_L+=B_ani
+                self.nicefieldL.append(nicefield)
             self.energy+=-Bani/2*(self.M.dot(syp.Matrix([ukx,uky,ukz])))**2
             
+
         
         self.pars[r"B_k"]=Bk
         self.pars['u_kx']=uk[0]
@@ -176,24 +210,50 @@ class macrospinSystem():
         
     def spinOrbitCoupling(self, lambdaSoc=0.01):
         lambda_soc, muB_L, muB_S = syp.symbols("lambda_LS, muB_L, muB_S")
-
+        e,muB = syp.symbols("e,mu_B")
 
         B_SOC=lambda_soc*q_e/muB
+        
+
 
         if "lambda_LS" not in self.variables:
-            self.energy+=lambda_soc*self.L.dot(self.S)
-            self.field_L+=B_SOC*muB_S*self.S
-            self.field_S+=B_SOC*muB_L*self.L
+            self.energy+=lambda_soc*self.ML.dot(self.MS)
+            self.field_L+=B_SOC*self.MS
+            self.field_S+=B_SOC*self.ML
+            self.nicefieldL.append(syp.MatMul(lambda_soc/muB,self.niceMS,evaluate=False))
+            self.nicefieldS.append(syp.MatMul(lambda_soc/muB,self.niceML,evaluate=False)) 
         
         self.pars["lambda_LS"] = lambdaSoc
     
     def getTimeEvolutions(self):
         Beff_s=self.field+self.field_S
         Beff_l=self.field+self.field_L
+        
+        gamma_base=-muB/hbar
 
-        dL_dt = (self.pars["g_l"]*muB/hbar*self.L.cross(Beff_l)+self.pars["g_l"]*muB/hbar*self.pars["alpha_L"]*self.L.cross(self.L.cross(Beff_l)))/(1+self.pars["alpha_L"]**2)
-        dS_dt = (self.pars["g_s"]*muB/hbar*self.S.cross(Beff_s)+self.pars["g_s"]*muB/hbar*self.pars["alpha_S"]*self.S.cross(self.S.cross(Beff_s)))/(1+self.pars["alpha_S"]**2)
-        return dL_dt, dS_dt     
+        dML_dt = self.pars["g_l"]*gamma_base/(1+self.pars["alpha_L"]**2)*(self.ML.cross(Beff_l)/mu0+self.pars["alpha_L"]/(self.pars["M_s"]*self.pars["f_L"])*self.ML.cross(self.ML.cross(Beff_l)/mu0))
+        dMS_dt = self.pars["g_s"]*gamma_base/(1+self.pars["alpha_S"]**2)*(self.MS.cross(Beff_s)/mu0+self.pars["alpha_S"]/(self.pars["M_s"]*self.pars["f_S"])*self.MS.cross(self.MS.cross(Beff_s)/mu0))
+        
+        return dL_dt, dS_dt 
+        
+    def dynamics(self):
+        gL,alphaL,gS,alphaS,mu_B,h_bar,mu_0,MsL,MsS = syp.symbols("g_L,alpha_L,g_S,alpha_S,mu_B,hbar,mu_0,M_s^L,M_s^S")
+        BeffL,BeffS,ML,MS,cross=syp.symbols('B_eff^L,B_eff^S,M_L,M_S,×', commutative=False)
+        niceBeff_s=syp.MatAdd(*self.nicefield, *self.nicefieldS, evaluate=False)
+        niceBeff_l=syp.MatAdd(*self.nicefield, *self.nicefieldL, evaluate=False)
+        gammaL=-gL*mu_B/(h_bar)
+        gammaS=-gS*mu_B/(h_bar)
+        dampL=1/(1+alphaL**2)
+        dampS=1/(1+alphaS**2)
+
+        nicedL_dT=syp.UnevaluatedExpr(gammaL*dampL*ML*cross*BeffL/mu_0+gammaL*alphaL*dampL/MsL*ML*cross*ML*cross*BeffL/mu_0)
+        nicedS_dT=syp.UnevaluatedExpr(gammaS*dampS*MS*cross*BeffS/mu_0+gammaS*alphaS*dampS/MsS*MS*cross*MS*cross*BeffS/mu_0)
+
+        
+        
+        return nicedL_dT, nicedS_dT
+        
+
     
     def getEquilibriumFunction(self, evaluate=True, returnSingle=True):
         TS=self.S.cross(self.field)
